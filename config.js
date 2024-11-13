ffd043557198a0fc01992aa33129451a2fedfbdd
manisha Sasmitha <manishasasmitha192@gmail.com>
2024-11-13 16:45:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "3yQWiZKa#lRb9ixQ_awC1pZQm-nOJYTkpv2LSBgmoy3GI3ka8Saw",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/eoqwjz.jpg",
 ALIVE_MS: process.env.ALIVE_MSG || "I'm Alive Now",
 };
