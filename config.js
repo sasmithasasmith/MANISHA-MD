66932fe856b329b58f55d86dc84c71a696422420
manisha Sasmitha <manishasasmitha192@gmail.com>
2024-11-14 22:05:23 +0530

    MANISHA-MD

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "y7ggDCzb#9D5TqnlfPzOzNRJ22dwZIp9Pufk7Uwu-RxazqgGIt1w",
+SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/6ctae3.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "*🎇WELLCOME TO MY MANISHA-MD BOT🎇*",
+ALIVE_MSG: process.env.ALIVE_MSG || "*🎇WELLCOME TO MANISHA-MD BOT🎇*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 };
